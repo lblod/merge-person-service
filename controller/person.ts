bda1a6d4dc9ce67eed37dc64986b0219b21e4029
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-03-03 11:39:24 +0100

    lmb-1257: fetch person uri by identifier

--- controller/person.ts
+++ controller/person.ts
@@ -1,15 +1,20 @@
-import { query, sparqlEscapeString } from 'mu';
+import { sparqlEscapeString } from 'mu';
+import { querySudo } from '@lblod/mu-auth-sudo';
 
 export async function getPersonById(id: string) {
   try {
-    const queryResult = await query(`
+    const queryResult = await querySudo(`
       PREFIX person: <http://www.w3.org/ns/person#>
       PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+      PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
   
       SELECT ?person
       WHERE {
-        ?person a person:Person .
-        ?person mu:uuid ${sparqlEscapeString(id)} .
+        GRAPH ?g {
+          ?person a person:Person .
+          ?person mu:uuid ${sparqlEscapeString(id)} .
+        }
+      ?g ext:ownedBy ?someone .
       } LIMIT 1
     `);
 
@@ -17,6 +22,35 @@ export async function getPersonById(id: string) {
   } catch (error) {
     throw {
       message: `Something went wrong while getting person with id: ${id}.`,
+      status: 500,
+    };
+  }
+}
+
+export async function getPersonByIdentifier(rrn: string) {
+  try {
+    const queryResult = await querySudo(`
+      PREFIX person: <http://www.w3.org/ns/person#>
+      PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+      PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+      PREFIX adms: <http://www.w3.org/ns/adms#>
+      PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+  
+      SELECT ?person
+      WHERE {
+        GRAPH ?g {
+          ?person a person:Person .
+          ?person adms:identifier ?identifier .
+          ?identifier skos:notation ${sparqlEscapeString(rrn)} .
+        }
+      ?g ext:ownedBy ?someone .
+      } LIMIT 1
+    `);
+
+    return queryResult.results.bindings[0].person?.value;
+  } catch (error) {
+    throw {
+      message: `Something went wrong while getting person with identifier: ${rrn}.`,
       status: 500,
     };
   }
--- router/person.ts
+++ router/person.ts
@@ -2,7 +2,7 @@ import Router from 'express-promise-router';
 
 import { Request, Response } from 'express';
 
-import { getPersonById } from '../controller/person';
+import { getPersonById, getPersonByIdentifier } from '../controller/person';
 
 export const personRouter = Router();
 
@@ -17,6 +17,19 @@ personRouter.get('/:id', async (req: Request, res: Response) => {
 
   res.status(200).send({
     id: req.params.id,
+    uri: personUri,
+  });
+});
+
+personRouter.get('/:rrn/identifier', async (req: Request, res: Response) => {
+  // eslint-disable-next-line no-useless-escape
+  const strippedRrn = req.params.rrn.replace(/[\.-]/g, '');
+  console.log({ rrn: req.params.rrn });
+  console.log({ strippedRrn });
+  const personUri = await getPersonByIdentifier(strippedRrn);
+
+  res.status(200).send({
+    rrn: strippedRrn,
     uri: personUri,
   });
 });
