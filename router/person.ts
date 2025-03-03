3ecc96ab7aae79afaa4a02beff647ab78a22dd6c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-03-03 16:43:29 +0100

    lmb-1257: check if the given values match the found person

--- router/person.ts
+++ router/person.ts
@@ -12,6 +12,28 @@ personRouter.post('/', async (req: Request, res: Response) => {
     createPersonRequest(req);
 
   const person = await getPersonByIdentifier(identifier);
+  const isCompleetMatch = [
+    firstName === person.firstName,
+    lastName === person.lastName,
+    birthDate.toJSON() === person.birthdate.toJSON(),
+  ].every((condition) => condition === true);
+
+  if (person.uri && !isCompleetMatch) {
+    throw {
+      message: 'We found a person for the identifier but the given values do not match.',
+      status: 404, // Statuscode: Not found
+    };
+  }
+
+  if (isCompleetMatch) {
+    throw {
+      message: 'The person you are trying to create already exists.',
+      status: 409, // Statuscode: Conflict
+    };
+  }
+
+  // BNo person found TODO:
+
 
   res.status(200).send(person);
 });
